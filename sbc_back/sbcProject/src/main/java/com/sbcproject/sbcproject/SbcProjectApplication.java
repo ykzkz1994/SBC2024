771917689cac82740ce3df7e0bcfdf0c942079e8
vanjooda <ukalliope@gmail.com>
2024-09-13 14:37:52 +0900

    first commit

---
+++ sbc_back/sbcProject/src/main/java/com/sbcproject/sbcproject/SbcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sbcproject.sbcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbcProjectApplication.class, args);
+    }
+
+}
---
+++ sbc_back/sbcProject/src/test/java/com/sbcproject/sbcproject/SbcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbcproject.sbcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
