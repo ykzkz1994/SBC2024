16095898d705209850c47bff2abfac6faa687f15
hgm <xxzz2563@naver.com>
2024-09-13 15:22:03 +0900

    2024-09-13 백앤드

---
+++ sbc_back/SbcCamping/src/main/java/com/sbcamping/SbcCampingApplication.java
@@ -0,0 +1,13 @@
+package com.sbcamping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcCampingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbcCampingApplication.class, args);
+    }
+
+}
