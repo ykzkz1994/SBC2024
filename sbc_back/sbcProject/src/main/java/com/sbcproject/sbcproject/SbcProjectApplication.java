f2cdde82c4c3ebef8061464c5be80f9ff2ddb74e
JinLee <leehyo9493@gmail.com>
2024-09-13 14:42:57 +0900

    240913.push.test

--- sbc_back/sbcProject/src/main/java/com/sbcproject/sbcproject/SbcProjectApplication.java
+++ sbc_back/sbcProject/src/main/java/com/sbcproject/sbcproject/SbcProjectApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbcProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SbcProjectApplication.class, args);
+
+        // test
+
     }
 
 }
