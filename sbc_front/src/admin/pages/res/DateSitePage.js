b135cfe638859ad348e780e24139ca2698608f94
J_Name <ykzkz1994@naver.com>
2024-09-24 23:47:14 +0900

    DateSitePage.js파일 현재 url판별해서 버튼 비활성화 기능

--- sbc_front/src/admin/pages/res/DateSitePage.js
+++ sbc_front/src/admin/pages/res/DateSitePage.js
@@ -1,34 +1,40 @@
-import React,{useState} from 'react'; //React=리액트 기본 라이브러리 ,{useState}여러 컴포넌트 교체를 위한 훅
-import DateSiteResList from '../../components/res/DateSiteResList'; //해당 페이지에서 보여줄 DateSiteResList를 import (달력형 예약확인  컴포넌트 )
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import DateSiteResList from '../../components/res/DateSiteResList';
+import { useNavigate, useLocation } from "react-router-dom";
 
-
-
-//함수
 const DateSitePage = () => {
+    const [currentComponent, setCurrentComponent] = useState('DateSitePage');
+    const navigate = useNavigate(); // 경로 이동을 위한 훅
+    const location = useLocation(); // 현재 경로 정보를 얻기 위한 훅
 
-    //컴포넌트 상태를 변경 할 수 있는 함수 기본값은 'TotalList'
-    const [currentComponent,setCurrentComponent]=useState('DateSitePage');
-
+    // 현재 경로와 비교하여 버튼을 비활성화할 조건
+    const isCurrentPage = (path) => location.pathname === path;
 
     return (
         <div>
-
             <div>
                 <div>
-                    <Link to={'/res/total'}>전체예약리스트</Link>
-                    <br/>
-                    <Link to={'/res/datesite'}>날짜 / 구역별 예약 리스트</Link>
+                    {/* 전체예약리스트 버튼 */}
+                    <button
+                        onClick={() => navigate('/res/total')}
+                        disabled={isCurrentPage('/res/total')}
+                    >
+                        전체예약리스트
+                    </button>
+                    <br />
+                    {/* 날짜 / 구역별 예약 리스트 버튼 */}
+                    <button
+                        onClick={() => navigate('/res/datesite')}
+                        disabled={isCurrentPage('/res/datesite')}
+                    >
+                        날짜 / 구역별 예약 리스트
+                    </button>
                 </div>
-
             </div>
             <h1>날짜/구역별 예약 리스트 페이지</h1>
 
-            <DateSiteResList/>
-
+            <DateSiteResList />
         </div>
-
-
     );
 };
 
