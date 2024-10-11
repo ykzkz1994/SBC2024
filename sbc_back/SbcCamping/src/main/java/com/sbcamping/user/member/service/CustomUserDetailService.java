4fcc2855e351f40cccd87d630b5f0f624581a9d5
JinLee <leehyo9493@gmail.com>
2024-10-11 12:40:24 +0900

    20241011 12:40 jin

--- sbc_back/SbcCamping/src/main/java/com/sbcamping/user/member/service/CustomUserDetailService.java
+++ sbc_back/SbcCamping/src/main/java/com/sbcamping/user/member/service/CustomUserDetailService.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailService implements UserDetailsService {
 
+    // Spring Security
+
     private final MemberRepository memberRepository;
 
     @Override
--- sbc_back/SbcCamping/src/main/java/com/sbcamping/user/member/service/MemberService.java
+++ sbc_back/SbcCamping/src/main/java/com/sbcamping/user/member/service/MemberService.java
@@ -5,9 +5,12 @@ import com.sbcamping.user.member.dto.MemberDTO;
 
 public interface MemberService {
     public void addMember(Member member);
-    public void updateMember(Long memberID, MemberDTO member);
+    public String updateMember(Long memberID, MemberDTO member);
     public void deleteMember(Long memberID);
     public Member getMember(Long memberId);
     public String emailCheck(String memberEmail);
     public String findEmail(String memberName, String memberPhone);
+    public Member findMemberByNameAndEmail(Member member);
+    public String updatePw(MemberDTO memberDTO);
+    public String authPw(Member member);
 }
