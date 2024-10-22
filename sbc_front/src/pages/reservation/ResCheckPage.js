fca5cf265b03952899ee338d9d11ecb0d0eb8a08
hgm <xxzz2563@naver.com>
2024-10-22 10:00:00 +0900

    2024-10-21 페이지만 만들면 끝

---
+++ sbc_front/src/pages/reservation/ResCheckPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResCheckPage = () => {
+    return (
+        <div>
+            <h1>예약완료페이지 만들어야지;;</h1>
+        </div>
+    );
+};
+
+export default ResCheckPage;
