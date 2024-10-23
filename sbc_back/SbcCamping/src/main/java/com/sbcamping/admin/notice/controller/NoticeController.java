eb3b35b85cd7301e42e0f88947e17093d7381976
vanjooda <155533103+vanjooda@users.noreply.github.com>
2024-10-23 16:50:13 +0900

    20241024 16:50 noticeController 재수정

--- sbc_back/SbcCamping/src/main/java/com/sbcamping/admin/notice/controller/NoticeController.java
+++ sbc_back/SbcCamping/src/main/java/com/sbcamping/admin/notice/controller/NoticeController.java
@@ -16,7 +16,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/notices") // 기본 URL 경로 설정
 @RequiredArgsConstructor // 의존성 주입을 위한 생성자 자동 생성
-
 public class NoticeController {
 
     //불변 인스턴스변수 선언
