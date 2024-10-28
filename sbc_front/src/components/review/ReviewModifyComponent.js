d1b0cea76d3650a14074281f42eee0a47071caf7
hgm <xxzz2563@naver.com>
2024-10-28 17:33:20 +0900

    2024 10-28 찐최종본

--- sbc_front/src/components/review/ReviewModifyComponent.js
+++ sbc_front/src/components/review/ReviewModifyComponent.js
@@ -199,6 +199,7 @@ const ReviewModifyComponent = () => {
                         value={review.reviewContent} // 내용
                         onChange={handleChangeReview}
                         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+
                         rows="5"
                     />
                 </div>
