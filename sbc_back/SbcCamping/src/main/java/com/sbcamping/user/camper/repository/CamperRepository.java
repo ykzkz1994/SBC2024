46d86e67057d2409c7da9256654bd511a3bcad8c
senogpyo0606 <psung0606@naver.com>
2024-10-31 11:36:45 +0900

    2024-10-30 jwt필터 검색 추가 11:36

--- sbc_back/SbcCamping/src/main/java/com/sbcamping/user/camper/repository/CamperRepository.java
+++ sbc_back/SbcCamping/src/main/java/com/sbcamping/user/camper/repository/CamperRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface CamperRepository extends JpaRepository<CamperBoard, Long> {
 
