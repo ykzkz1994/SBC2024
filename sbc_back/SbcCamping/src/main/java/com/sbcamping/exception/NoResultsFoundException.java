6ab43d33723c55ea81911c65c4ca98a2b931e847
JOUNGSANGHO <ykzkz1994@naver.com>
2024-10-31 17:02:16 +0900

    ppt

--- sbc_back/SbcCamping/src/main/java/com/sbcamping/exception/NoResultsFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sbcamping.exception;
-
-public class NoResultsFoundException extends RuntimeException {
-    public NoResultsFoundException(String message) {
-        super(message);
-    }
-}
